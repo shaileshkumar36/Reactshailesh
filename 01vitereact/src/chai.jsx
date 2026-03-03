59b54caebde5ad5c404898b3321e18d5cc40c7f6
shaileshkumar36git config --global user.email shailesraj7857@gmail.com <shaileshraj7857@gmail.com>
2026-03-03 15:40:14 +0530

    ..

--- 01vitereact/src/chai.jsx
+++ 01vitereact/src/chai.jsx
@@ -1,7 +1,9 @@
 function Chai(){
     return (
+        <>
         <h1> what happened my dear this my new file which is import App.jsx </h1>
+        <h4>;alkdsf;alsdkjf</h4>
+        </>
     )
 }
-
 export default Chai;
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-//imort Chai from './chai.jsx';
+
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom App | shailesh</h1>
+        </div>
+    )
+}
+const anotherElement = (
+    <a href = "https://google.com" target='_blank'>
+        Visit goole</a>
+)
+
+const reactElement = React.createElement(
+    'a',
+    {href:'https://google.com',target:'_blank'},
+    'click me to visit google'
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    //reactElement 
+
+    
 )
